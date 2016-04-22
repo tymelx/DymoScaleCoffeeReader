855e7f8b93699ed79a6defb85ce21f68d7dd78e5
Justin Valerius <Justin Valerius>
2016-04-21 21:24:46 -0500

    now recording stable weight and processing after 3 seconds. made configuration vars at the top.

--- scale_reader.py
+++ scale_reader.py
@@ -8,6 +8,23 @@ import gtk
 from sys import exit
 import math
 
+#Configurations!
+#Modes for the Dymo M25 g/oz
+DATA_MODE_GRAMS = 2
+DATA_MODE_OUNCES = 11
+
+#Device IDs - lsusb to verify this is corrects
+# DYMO M25
+VENDOR_ID = 0x0922
+PRODUCT_ID = 0x8003
+
+TIME_RECONNECT = 5
+
+#Take the measurements for the pot and give them here!
+DATA_EMPTY_POT_WEIGHT_GRAMS = 10 #1950 #technically 1938 - but a little rounding for that half cup won't hurt
+#could be more accurate for ounces - probably will be in the future
+DATA_EMPTY_POT_WEIGHT_OUNCES = 20 #19.5 technically - but the math.ceil will account for this!
+
 def main():
     try:
         dev = connect_to_scale()
@@ -20,35 +37,29 @@ def main():
             usb.util.claim_interface(dev, interface) #hostile takeover
 
         #read away!
-        read_from_scale(dev)
+        listen_on_scale(dev)
             
     except KeyboardInterrupt as e:
             print "\nQuitting.."
             exit()
 
 def connect_to_scale():
-    # DYMO M25
-    VENDOR_ID = 0x0922
-    PRODUCT_ID = 0x8003
-    
     print("Attempting to connect to scale...")
     dev = usb.core.find(idVendor=VENDOR_ID,
                        idProduct=PRODUCT_ID)
     
     if dev is None:
-        time.sleep(5.0)
+        time.sleep(TIME_RECONNECT)
         return connect_to_scale()
     else:
         return dev
 
-def read_from_scale(dev):
-    DATA_MODE_GRAMS = 2
-    DATA_MODE_OUNCES = 11
-        
+def listen_on_scale(dev):
     previous_weight = 0
     weight = 0
+    total_time_same_weight = 0
     
-    print "listening for weight..."
+    print "Listening for weight..."
 
     while True:
         time.sleep(.5)
@@ -59,24 +70,39 @@ def read_from_scale(dev):
         data = read_scale_weight(dev)
 
         if data == "ERROR":
-            print("Device lost, disconnecting")
+            print("Device lost, will attempt to reconnect...")
             main() #restart the program!
             break
         
         if data != None:
             weight = data[4] + data[5] * 256
         
+        #Once it's the same weight for 3 seconds - we'll process that as "stable" and do some operations
         if weight == previous_weight:
+            total_time_same_weight += 0.5
+            if total_time_same_weight == 3.0:
+                print("Stabalized!")
+                process_stable_weight(data[2], convert_scale_weight(weight))
+            
             continue
+        else:
+            total_time_same_weight = 0
 
         previous_weight = weight
-        
-        if data[2] == DATA_MODE_OUNCES:
-            weight = math.ceil((weight * 0.1))
-        elif data[2] == DATA_MODE_GRAMS:
-            weight = math.ceil(weight)
+        #if data[2] == DATA_MODE_OUNCES:
+        #    weight = math.ceil((weight * 0.1))
+        #elif data[2] == DATA_MODE_GRAMS:
+        #    weight = math.ceil(weight)
 
-        process_scale_weight(data[2], weight)
+        #process_scale_weight(data[2], weight)
+
+def convert_scale_weight(raw_weight, mode):
+    if mode == DATA_MODE_OUNCES:
+        return math.ceil((raw_weight * 0.1))
+    elif mode == DATA_MODE_GRAMS:
+        return math.ceil(raw_weight)
+        
+    return weight #failsafe
 
 
 def read_scale_weight(dev):
@@ -109,15 +135,9 @@ def read_scale_weight(dev):
     except IndexError as e:
         print "IndexError: " + str(e.args)
 
-def process_scale_weight(mode, weight):
-    DATA_EMPTY_POT_WEIGHT_GRAMS = 10 #1950 #technically 1938 - but a little rounding for that half cup won't hurt
-    DATA_EMPTY_POT_WEIGHT_OUNCES = 20 #19.5 technically - but the math.ceil will account for this!
-    
-    DATA_MODE_GRAMS = 2
-    DATA_MODE_OUNCES = 11
-
+def process_stable_weight(mode, weight):
     if weight == 0:
-        print("No coffee pot on the scale")
+        print("No pot on the scale, must be getting refilled (or it better be!")
     else:
         empty_pot = False
         weight_type = None
@@ -132,9 +152,6 @@ def process_scale_weight(mode, weight):
             remaining_coffee = weight - DATA_EMPTY_POT_WEIGHT_GRAMS
             
         if empty_pot is True:
-            print("No more coffee")
-            
+            print("No more coffee, someone refill me!")
         else:
             print "{} {} of coffee remaining".format(remaining_coffee, weight_type)
-
-main()
