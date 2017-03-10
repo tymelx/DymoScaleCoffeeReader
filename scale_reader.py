b422fa31a20cf8ad302136d3c15ca1c9966bc532
Justin Valerius <tymelx@gmail.com>
2017-03-10 21:41:43 +0000

    Refactored scale coffee reader to be more readable - no more bloat code

--- scale_reader.py
+++ scale_reader.py
@@ -8,17 +8,20 @@ import gtk
 from sys import exit
 import math
 
+DATA_MODE_GRAMS = 2
+DATA_MODE_OUNCES = 12
+
+# DYMO S400
+VENDOR_ID = 0x0922
+PRODUCT_ID = 0x8009
+
+IS_CONNECTED_TO_SCALE = False
+
 def main():
     try:
         dev = connect_to_scale()
-
-        interface = 0
-        if dev.is_kernel_driver_active(interface) is True:
-            print("Detaching kernel driver...")
-            dev.detach_kernel_driver(interface)
-            dev.set_configuration() #default configs
-            usb.util.claim_interface(dev, interface) #hostile takeover
-
+        hostile_takeover_scale(dev)
+        
         #read away!
         read_from_scale(dev)
             
@@ -26,11 +29,15 @@ def main():
             print "\nQuitting.."
             exit()
 
+def hostile_takeover_scale(dev):
+    interface = 0
+    if dev.is_kernel_driver_active(interface) is True:
+        print("Detaching kernel driver...")
+        dev.detach_kernel_driver(interface)
+        dev.set_configuration() #default configs
+        usb.util.claim_interface(dev, interface) #hostile takeover
+
 def connect_to_scale():
-    # DYMO M25
-    VENDOR_ID = 0x0922
-    PRODUCT_ID = 0x8003
-    
     print("Attempting to connect to scale...")
     dev = usb.core.find(idVendor=VENDOR_ID,
                        idProduct=PRODUCT_ID)
@@ -42,23 +49,23 @@ def connect_to_scale():
         return dev
 
 def read_from_scale(dev):
-    DATA_MODE_GRAMS = 2
-    DATA_MODE_OUNCES = 11
-        
+    sleep_time = .5
+    stable_sleep_time = 3
     previous_weight = 0
     weight = 0
+
+    total_time_at_weight = 0
     
     print "listening for weight..."
 
     while True:
-        time.sleep(.5)
+        time.sleep(sleep_time)
 
         weight = 0
-        print_weight = ""
 
         data = read_scale_weight(dev)
 
-        if data == "ERROR":
+        if data is None:
             print("Device lost, disconnecting")
             main() #restart the program!
             break
@@ -67,16 +74,17 @@ def read_from_scale(dev):
             weight = data[4] + data[5] * 256
         
         if weight == previous_weight:
+            total_time_at_weight += sleep_time
+            if total_time_at_weight == stable_sleep_time:
+                process_scale_weight(data[2], weight, True)
             continue
+        else:
+            total_time_at_weight = 0
+            preview_weight = weight
+            process_scale_weight(data[2], weight, False)
 
+        #only process weights that arent the same as the previ
         previous_weight = weight
-        
-        if data[2] == DATA_MODE_OUNCES:
-            weight = math.ceil((weight * 0.1))
-        elif data[2] == DATA_MODE_GRAMS:
-            weight = math.ceil(weight)
-
-        process_scale_weight(data[2], weight)
 
 
 def read_scale_weight(dev):
@@ -87,21 +95,14 @@ def read_scale_weight(dev):
         # read a data packet
         attempts = 0
         data = None
+        
         while data is None and attempts < 10:
             try:
                 data = dev.read(endpoint.bEndpointAddress,
                                    endpoint.wMaxPacketSize)
             except usb.core.USBError as e:
+                print(str(e))
                 data = None
-                #if e.args == ('Operation timed out',):
-                #    print("IM HERE")
-                #    attempts -= 1
-                #    print "timed out... trying again"
-                #    continue
-                #else:
-                #    print(e.args)
-                #Assume any exception is bad and just tick up the attempts
-                return "ERROR"
 
         return data
     except usb.core.USBError as e:
@@ -109,32 +110,9 @@ def read_scale_weight(dev):
     except IndexError as e:
         print "IndexError: " + str(e.args)
 
-def process_scale_weight(mode, weight):
-    DATA_EMPTY_POT_WEIGHT_GRAMS = 10 #1950 #technically 1938 - but a little rounding for that half cup won't hurt
-    DATA_EMPTY_POT_WEIGHT_OUNCES = 20 #19.5 technically - but the math.ceil will account for this!
-    
-    DATA_MODE_GRAMS = 2
-    DATA_MODE_OUNCES = 11
-
-    if weight == 0:
-        print("No coffee pot on the scale")
-    else:
-        empty_pot = False
-        weight_type = None
-        remaining_coffee = 0
-        if mode == DATA_MODE_OUNCES:
-            empty_pot = weight <= DATA_EMPTY_POT_WEIGHT_OUNCES
-            weight_type = "ounces"
-            remaining_coffee = weight - DATA_EMPTY_POT_WEIGHT_OUNCES
-        elif mode == DATA_MODE_GRAMS:
-            empty_pot  = weight <= DATA_EMPTY_POT_WEIGHT_GRAMS
-            weight_type = "grams"
-            remaining_coffee = weight - DATA_EMPTY_POT_WEIGHT_GRAMS
-            
-        if empty_pot is True:
-            print("No more coffee")
-            
-        else:
-            print "{} {} of coffee remaining".format(remaining_coffee, weight_type)
+def process_scale_weight(mode, weight, stable):
+    weight = weight * .1
+    formatted_weight_mode = "pounds" if mode == DATA_MODE_OUNCES else "kilograms"
+    print("STABLE = " + str(stable) + " Weight is currently: " + str(weight) + " " + formatted_weight_mode)
 
 main()
